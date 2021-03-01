e3b371a28c107819c3f01feeefd0edfadf6a66ec
kelvia-snts <Kelviaks@gmail.com>
2021-03-01 16:48:20 -0300

    exercícios funções

---
+++ semana4/funcoes/js-vanilla-template/src/index.js
@@ -0,0 +1,101 @@
+/*Exercícios de interpretação de código
+
+1. 
+
+a. 10 e 50, pois a função pede que a variável atribuída seja mulplicada por 5
+b. Nada. so o comando console.log permite apresentar as informações geradas pelo código 
+
+2. 
+
+a. As saídas serão: Darvas e Caio
+b. Novamente os dois primeiros nomes: Amanada e Caio 
+
+3. ?? melhor nome talvez seria: novoArray
+
+*/
+
+/* Exercícios de escrita de código
+
+4.
+ a.
+ const sobreMim = function(){
+   console.log('Eu sou Kelvia, tenho 31 anos, moro em Olinda-PE e sou estudante da Labenu.')
+ }
+
+b.
+ let nome = prompt('Qual seu nome?')
+ let idade = Number(prompt('Qual a sua idade?'))
+ let cidade = prompt('Onde você mora?')
+ let estudante = prompt('Você é estudante? Digite Sim ou Não')
+
+
+const apresentacao = () => {
+  console.log('Eu sou', nome, ', tenho', idade, ' anos, moro em', cidade,'e', estudante)
+}
+
+5.
+
+a. (ok?)
+let variavel1= Number(prompt('Digite um número'))
+let variavel2= Number(prompt('Digite outro número'))
+
+function soma() {
+  return variavel1 + variavel2
+}
+console.log(soma())
+
+b. (ok?) 
+let entrada1= Number(prompt('Digite um número'))
+let entrada2= Number(prompt('Digite outro número'))
+
+function booleana(){
+  if (entrada1 >= entrada2){
+  return true
+  } else{
+    return false
+  }
+}
+console.log('Os números digitados foram: ', entrada1, entrada2)
+console.log('O primeiro número é maior ou igual ao primeiro? ', booleana())
+
+c.(ok?)
+
+let mensagem = prompt (' Digite uma mensagem: ')
+
+function repete() {
+  return mensagem.repeat(10) 
+}
+console.log(repete())
+
+6.
+a.( ok?)  
+const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+function numeros() {
+  return array.length
+}
+console.log(numeros())
+
+b. (ok?) 
+let numero = Number(prompt('escolha um número: '))
+
+function booleano(){
+  if (numero %2===0) {
+    return true
+  }else {
+    return false
+  }
+}
+console.log('O numero é par? ', booleano())
+
+c.
+*/
+
+const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+
+function pares(){
+  
+}
+
+
+
+ 
