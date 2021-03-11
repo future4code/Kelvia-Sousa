15632a9c580922ebf40380b4f55b1ff2a762c269
KeLvIA KeLlInE <kelviaks@gmail.com>
2021-03-11 15:36:00 -0300

    iniciando exercício aula 21

--- semana5/insta4/insta4 2/src/App.js
+++ semana5/insta4/insta4 2/src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+  state = {
+    Post: [
+      nomeUsuario: 'paulinha'
+      fotoUsuario: 'https://picsum.photos/50/50'
+      fotoPost: 'https://picsum.photos/200/150'
+    ]
+  }
+
   render() {
     return (
       <div className={'app-container'}>
