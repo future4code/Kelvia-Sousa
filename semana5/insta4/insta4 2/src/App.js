e4c3364c4720af409e8d93661e06f3019713504b
KeLvIA KeLlInE <kelviaks@gmail.com>
2021-03-11 16:35:16 -0300

    lista para renderizar componentes

--- semana5/insta4/insta4 2/src/App.js
+++ semana5/insta4/insta4 2/src/App.js
@@ -4,34 +4,40 @@ import Post from './components/Post/Post';
 
 class App extends React.Component {
   state = {
-    Post: [
-      nomeUsuario: 'paulinha'
-      fotoUsuario: 'https://picsum.photos/50/50'
-      fotoPost: 'https://picsum.photos/200/150'
+    posts: [
+      {
+      nomeUsuario: 'paulinha',
+      fotoUsuario: 'https://picsum.photos/50/50',
+      fotoPost: 'https://picsum.photos/200/150',
+      },
+
+      {
+      nomeUsuario: 'Kel_via',
+      fotoUsuario: 'https://media-exp1.licdn.com/dms/image/C4D03AQFTz3PaYBvizg/profile-displayphoto-shrink_200_200/0/1608637456838?e=1620864000&v=beta&t=frJiABA0TDE1fIyR64HW_Gn_kr2a3dmOwdYDdaSirg0',
+      fotoPost: 'https://picsum.photos/300/150',
+      },
+
+      {
+      nomeUsuario: 'Edite',
+      fotoUsuario: 'https://i.pinimg.com/236x/f3/28/8c/f3288cdb177e602aa28c15291ef7407d.jpg',
+      fotoPost: 'https://picsum.photos/140/150',
+      },
     ]
   }
 
   render() {
+    const ListaDeComponentes= this.state.posts.map((post) => {
     return (
-      <div className={'app-container'}>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'Kel_via'}
-          fotoUsuario={'https://media-exp1.licdn.com/dms/image/C4D03AQFTz3PaYBvizg/profile-displayphoto-shrink_200_200/0/1608637456838?e=1620864000&v=beta&t=frJiABA0TDE1fIyR64HW_Gn_kr2a3dmOwdYDdaSirg0'}
-          fotoPost={'https://picsum.photos/300/150'}
-        />
-        <Post
-          nomeUsuario={'Edite'}
-          fotoUsuario={'https://i.pinimg.com/236x/f3/28/8c/f3288cdb177e602aa28c15291ef7407d.jpg'}
-          fotoPost={'https://picsum.photos/140/150'}
-        />
-      </div>
+    <Post nomeUsuario={post.nomeUsuario} fotoUsuario={post.fotoUsuario} fotoPost={post.fotoPost}/>
+    )
+    })
+
+    return (
+    <div className={'app-container'}> 
+    {ListaDeComponentes}
+    </div>
     );
   }
 }
 
-export default App;
+export default App 
