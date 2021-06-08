d311e5e59f7c189c7c954474ecd7b22e83045729
kelvia sousa <kelviaks@gmail.com>
2021-06-08 15:14:29 -0300

    exercícios e desafios aula 52

--- semana18/template-backend/src/index.ts
+++ semana18/template-backend/src/index.ts
@@ -1,12 +1,17 @@
 import app from "./app"
-import editUser from './endpoints/editUser'
-import createUser from './endpoints/createUser'
+// import editUser from './endpoints/editUser'
+import createUser from './endpoints/createUser';
+import deleteUser from "./endpoints/deleteUser";
+import getProfile from "./endpoints/getProfile";
+import getUser from "./endpoints/getUser";
 import login from "./endpoints/login";
 // import { compareHash, createHash } from "./services/hashManager";
 
-app.post('/user/signup', createUser);
-app.post('/user/login', login);
-app.put('/user/edit/', editUser);
+app.post('/user/signup', createUser );
+app.post('/user/login', login );
+app.get('/user/profile', getProfile);
+app.get('/user/:id', getUser);
+app.delete('/user/:id' , deleteUser);
 
 // const hash1 = createHash("senha")
 // const hash2 = createHash("senha")
